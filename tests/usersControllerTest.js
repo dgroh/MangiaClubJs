549f32fc4ccdbb46d4e0cf8f4bfe7f4e97096ba7
Daniel Groh <daniel.groh@iti-software.de>
2020-07-13 07:57:13 +0200

    style: fix linting

--- tests/usersControllerTest.js
+++ tests/usersControllerTest.js
@@ -1,51 +1,47 @@
-const should = require('should');
-const sinon = require('sinon');
-const usersController = require('../controllers/usersController');
-
 beforeEach(async () => {
 
 });
 
 describe('usersController', () => {
-    describe('Get', () => {
-        it("should return http code 404 when user not found", async () => {
+  describe('Get', () => {
+    it('should return http code 404 when user not found', async () => {
 
-        });
-
-        it("should get all users", async () => {
-
-        });
-
-        it("should get user by id", async () => {
-
-        });
     });
 
-    describe('Put', () => {
-        it("should return http code 403 when token not present", async () => {
+    it('should get all users', async () => {
 
-        });
-
-        it("should return http code 500 when token expired", async () => {
-
-        });
-
-        it("should update user", async () => {
-
-        });
     });
 
-    describe('Post', () => {
-        it("should return error when request is not valid", async () => {
+    it('should get user by id', async () => {
 
-        });
-
-        it("should return http code 409 when user exists", async () => {
-
-        });
-
-        it("should create user", async () => {
-
-        });
     });
-}); 
+  });
+
+  describe('Put', () => {
+    it('should return http code 403 when token not present', async () => {
+
+    });
+
+    it('should return http code 500 when token expired', async () => {
+
+    });
+
+    it('should update user', async () => {
+
+    });
+  });
+
+  describe('Post', () => {
+    it('should return error when request is not valid', async () => {
+
+    });
+
+    it('should return http code 409 when user exists', async () => {
+
+    });
+
+    it('should create user', async () => {
+
+    });
+  });
+});
